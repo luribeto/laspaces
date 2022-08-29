dcc5109169b57961356b236ddb87c0821c1151ac
Leonardo Uribe <luribeto@192.168.68.106>
2022-08-28 20:59:08 -0500

    unused vars

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import paces from './paces-v.jpeg';
+// import logo from './logo.svg';
+// import paces from './paces-v.jpeg';
 import './App.css';
 
 function App() {
