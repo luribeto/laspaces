5b8350f5c948e73286bbb28b4dee2a5d1080a0e4
Leonardo Uribe <luribeto@192.168.68.106>
2022-08-29 19:32:32 -0500

    new

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 // import paces from './paces-v.jpeg';
+import logo from './laspaces-logo-2.png';
 import './App.css';
 
 function App() {
@@ -7,19 +8,22 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="App-background">
-          {/* <img src={paces} className="App-background" alt="peace"/> */}
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <div className="App-texts">
-            <h2>LAS <b>PACES</b></h2>
-            {/* <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Especialistas en justicia restaurativa.
-            </a> */}
-            <h5>Especialistas en Justicia Restaurativa.</h5>
+        <div className="App-overlay">
+            {/* <img src={paces} className="App-background" alt="peace"/> */}
+            {/* <img src={logo} className="App-logo" alt="logo" /> */}
+            <div className="App-texts">
+              {/* <img src={logo} className="App-logo-2" alt="peaces"/> */}
+              <h2>LAS <b>PACES</b></h2>
+              {/* <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Especialistas en justicia restaurativa.
+              </a> */}
+              <h5>Especialistas en Justicia Restaurativa.</h5>
+            </div>
           </div>
         </div>
       </header>
