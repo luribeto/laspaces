65c6192e480f66482ffa530fce9902e329ba5cf9
Leonardo Uribe <luribeto@192.168.68.105>
2022-09-20 18:02:51 -0500

    rsponsive testimonials

--- src/components/ui-components/roundImg/roundImg.jsx
+++ src/components/ui-components/roundImg/roundImg.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // Scss
 import "./roundImg.scss";
+import { Row, Col } from "react-flexbox-grid";
 
 
 const roundImg = (props) => {
@@ -9,17 +10,21 @@ const roundImg = (props) => {
 
   return (
     <div className="testomonio" onClick={onClick}>
-      <img src={image} alt="Testimonio" />
-      <div>
-        <h3 className="font30 weight800 padding5">{title}</h3>
-        <h5 className="font20 weight800 margin0">{role}</h5>
-        <p>
-          <span className="quote">"</span>
-          {description}
-          <span className="quote">"</span>
-        </p>
-        <i>{date}</i>
-      </div>
+      <Row>
+        <Col xs={12} sm={12} md={4} lg={3} className="testimonial-img">
+          <img src={image} alt="Testimonio" />
+        </Col>
+        <Col xs={12} sm={12} md={6} lg={7} className="testimonial-txt">
+          <h3 className="font30 weight800 padding5">{title}</h3>
+          <h5 className="font20 weight800 margin0">{role}</h5>
+          <p>
+            <span className="quote">"</span>
+            {description}
+            <span className="quote">"</span>
+          </p>
+          <i>{date}</i>
+        </Col>
+      </Row>
     </div>
   );
 };
